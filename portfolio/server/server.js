5ce20f940debf9a8ff95e189362b60b5fc711529
Udai Raj Pandey <149392493+Raj9555@users.noreply.github.com>
2026-03-14 14:06:36 +0530

    transporter changed

--- portfolio/server/server.js
+++ portfolio/server/server.js
@@ -61,9 +61,9 @@ app.post("/send", async (req, res) => {
 
     try {
         const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: Number(process.env.SMTP_PORT),
-            secure: false,
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS,
