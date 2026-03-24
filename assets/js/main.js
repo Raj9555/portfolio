e743b9a91fa2158959a21ec92057c94272d3d270
Udai Raj Pandey <149392493+Raj9555@users.noreply.github.com>
2026-03-24 14:07:06 +0530

    Refactor chat functions and add scroll active feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,33 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+const sections = document.querySelectorAll("section[id]")
+
+function scrollActive() {
+
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 80
+        const sectionId = current.getAttribute("id")
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+
+            document
+                .querySelector(".nav__menu a[href*=" + sectionId + "]")
+                .classList.add("active-link")
+
+        } else {
+
+            document
+                .querySelector(".nav__menu a[href*=" + sectionId + "]")
+                .classList.remove("active-link")
+
+        }
+
+    })
+}
+
+window.addEventListener("scroll", scrollActive)
